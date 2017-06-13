e41a7336e105978549e2bceab2187d2b46c507df
Christopher Jordan <christopherjordan87@gmail.com>
2017-06-13 11:01:16 +0800

    New simple script to get the finished, successful obsids and their timestamps.

---
+++ scripts/gator_read_table_only_peeled.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require "sqlite3"
+require "optparse"
+
+options = {}
+OptionParser.new do |opts|
+    opts.banner = "Usage: #{__FILE__} [-d /path/to/database]\nDisplay the contents of a database."
+    opts.on("-h", "--help", "Display this message.") {puts opts; exit}
+
+    $database = "#{ENV["MYGROUP"]}/obsids.sqlite"
+	opts.on("-d", "--database DATABASE", "Specify the database to be used. By default, this is #{$database}") {|o| $database = o}
+end.parse!
+
+# Multiple tables can be used in the same database, but for now assume it's always the same one.
+table_name = "downloads"
+
+begin
+    abort("Database (#{$database}) doesn't exist!") unless File.exists?($database)
+    db = SQLite3::Database.open $database
+    db.results_as_hash = true
+
+    db.execute("select * from #{table_name}") do |r|
+        if r["Status"] == "peeled"
+            puts "#{r["Obsid"]}/#{r["Timestamp"]}"
+        end
+    end
+
+rescue SQLite3::Exception => e
+    puts "#{e.class}: #{e}"
+
+ensure
+    db.close if db
+end
