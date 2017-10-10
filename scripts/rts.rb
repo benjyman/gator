54adea4429c90c66b040cc321184e4a26ae119c2
Christopher Jordan <christopherjordan87@gmail.com>
2017-10-10 15:59:27 +0800

    Made object-oriented code. Should now handle picket-fence data as well as normal data. Other scripts probably need to be modified to work with the new framework.

--- scripts/rts.rb
+++ scripts/rts.rb
@@ -16,15 +16,33 @@ OptionParser.new do |opts|
 
     $run_time = 40
 	opts.on("-t", "--run_time NUM", "Allow the RTS to run for this many minutes. Default: #{$run_time}") {|o| $run_time = o.to_i}
+
+    $patch = true
+	opts.on("--patch", "Disable the patch step. Default: #{$patch}") {|o| $patch = false}
+
+    $peel = true
+	opts.on("--peel", "Disable the peel step. Default: #{$peel}") {|o| $peel = false}
 end.parse!
 
+
+# Sanity checks.
+# We can't peel if we're not patching.
+if $peel and not $patch
+    abort "Cannot peel if we are not patching; exiting."
+end
 
 if __FILE__ == $0
     abort "No obsids supplied!" if ARGV.length == 0
 
-    obtain_obsids(ARGV).each do |o|
-        setup_jobid, timestamp = rts_setup(o, peel_number: $peel_number)
-        puts setup_jobid
-        puts patch_jobid = rts_patch(o, setup_jobid, timestamp, mins: $run_time, peel: true)
+    obtain_obsids(ARGV).each do |obsid|
+        o = Obsid.new(obsid)
+        o.rts(setup_mins: 5,
+              cal_mins: $run_time,
+              patch: true,
+              peel: true,
+              peel_number: $peel_number,
+              timestamp: true,
+              rts_path: "/group/mwaeor/CODE/RTS/bin/rts_gpu")
+        puts "#{obsid}: #{o.type}"
     end
 end
